4143f6bef84698c1e765fc2adb0ab1b588c1158f
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2022-01-31 18:37:13 +0100

    Update main.py
    main.py

--- main.py
+++ main.py
@@ -3,24 +3,83 @@ from helper import manager
 app = Flask(__name__, template_folder="templates")
 
 
-cargo = {"rower": 5, "traktor": 10, "samolot": 1}
-
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('index.html', cargo=cargo)
+    cargo = manager.warehouse
+    balance = manager.balance
+    return render_template('index.html', cargo=cargo, balance=balance)
 
 
-@app.route("/ksiegowosc")
-def web_account():
-    saldo = manager.balance
-    return render_template('account.html', saldo=saldo, title='księgowość')
+@app.route("/saldo")
+def balance():
+    return render_template("balance.html", title="Saldo")
 
 
-@app.route("/archiwum")
-def archive():
+@app.route("/balance_response", methods=['POST', 'GET'])
+def balance_response():
+    manager.error = ""
+    income = int(request.form.get('income'))
+    comment = request.form.get('comment')
+    manager.execute("saldo", income, comment)
+    error = manager.error
+    cargo = manager.warehouse
+    balance = manager.balance
+    manager.write_data_to_file()
+    return render_template("balance_response.html", income=income, comment=comment, error=error,
+                           cargo=cargo, balance=balance)
+
+
+@app.route("/zakup")
+def buy():
+    return render_template("buy.html", title="Zakup towaru")
+
+
+@app.route("/buy_response", methods=['POST', 'GET'])
+def buy_response():
+    manager.error = ""
+    item = request.form.get('item')
+    price = int(request.form.get('price'))
+    quantity = int(request.form.get('quantity'))
+    manager.execute("zakup", item, price, quantity)
+    error = manager.error
+    cargo = manager.warehouse
+    balance = manager.balance
+    manager.write_data_to_file()
+    return render_template("buy_response.html", item=item, price=price, quantity=quantity, error=error,
+                           cargo=cargo, balance=balance)
+
+
+@app.route("/sprzedaz")
+def sale():
+    cargo = manager.warehouse
+    return render_template("sale.html", title="Sprzedaż towaru", cargo=cargo)
+
+
+@app.route("/sale_response", methods=['POST', 'GET'])
+def sale_response():
+    manager.error = ""
+    item = request.form.get('item')
+    price = int(request.form.get('price'))
+    quantity = int(request.form.get('quantity'))
+    manager.execute("sprzedaz", item, price, quantity)
+    error = manager.error
+    cargo = manager.warehouse
+    balance = manager.balance
+    manager.write_data_to_file()
+    return render_template("sale_response.html", item=item, price=price, quantity=quantity, error=error,
+                           cargo=cargo, balance=balance)
+
+
+@app.route("/historia")
+@app.route("/historia/<line_from>/<line_to>")
+def archive(line_from=None, line_to=None):
     log = manager.logs
-    return render_template('archive.html', title='archiwum', log=log)
+    if line_from and line_to:
+        return render_template('archive.html', title=f"historia od {line_from} do {line_to}",
+                               log=log, line_from=int(line_from), line_to=int(line_to))
+    else:
+        return render_template('archive.html', title='historia', log=log, line_from=0, line_to=len(log))
 
 
 @app.errorhandler(404)
